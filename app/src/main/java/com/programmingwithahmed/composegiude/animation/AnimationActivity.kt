51c1beee6e150d4b91bac06fdf2c276b04338ff0
Ahmed <a.ramadan.fln@ncgr.gov.sa>
2023-06-03 00:58:26 +0300

    Add Cross fade animation

--- app/src/main/java/com/programmingwithahmed/composegiude/animation/AnimationActivity.kt
+++ app/src/main/java/com/programmingwithahmed/composegiude/animation/AnimationActivity.kt
@@ -14,7 +14,7 @@ class AnimationActivity : AppCompatActivity() {
 
         setContent {
             ComposeGiudeTheme {
-                AnimationSizeContentWithSpecScreen()
+                AnimationCrossFadeScreen()
             }
         }
     }
