57350646b37cb49fd6a0ec415c9a59c9ff54b97f
mostafaan <mostafaan@aecl.com>
2023-06-18 15:21:46 +0300

    Finish Onboard example

--- app/src/main/java/com/programmingwithahmed/composegiude/onboard/OnboardActivity.kt
+++ app/src/main/java/com/programmingwithahmed/composegiude/onboard/OnboardActivity.kt
@@ -2,27 +2,31 @@ package com.programmingwithahmed.composegiude.onboard
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.getValue
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
-import com.programmingwithahmed.composegiude.onboard.onboard.route.ROUTE
 import com.programmingwithahmed.composegiude.ui.theme.ComposeGiudeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class OnboardActivity : AppCompatActivity() {
 
+    private val splashViewModel: SplashViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         installSplashScreen().setKeepOnScreenCondition{
-            true
+            splashViewModel.isLoading.value
         }
 
         setContent {
             ComposeGiudeTheme {
+                val startScreen by splashViewModel.startDestination
                 val navController = rememberNavController()
-                NavGraph(navController, ROUTE)
+                NavGraph(navController, startScreen)
             }
         }
     }
--- app/src/main/java/com/programmingwithahmed/composegiude/onboard/SplashViewModel.kt
+++ app/src/main/java/com/programmingwithahmed/composegiude/onboard/SplashViewModel.kt
@@ -1,32 +1,43 @@
 package com.programmingwithahmed.composegiude.onboard
 
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.programmingwithahmed.composegiude.onboard.data.GetOnboardStatusUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
+@HiltViewModel
 class SplashViewModel @Inject constructor(
     private val getOnboardStatusUseCase: GetOnboardStatusUseCase
-): ViewModel() {
+) : ViewModel() {
 
     private val _isLoading: MutableState<Boolean> = mutableStateOf(true)
     val isLoading: State<Boolean> = _isLoading
 
-    private val _startDestination: MutableState<String> = mutableStateOf(Screen.Welcome.route)
+    private val _startDestination: MutableState<String> = mutableStateOf("")
     val startDestination: State<String> = _startDestination
 
     init {
-        viewModelScope.launch {
-            repository.readOnBoardingState().collect { completed ->
-                if (completed) {
-                    _startDestination.value = Screen.Home.route
-                } else {
-                    _startDestination.value = Screen.Welcome.route
-                }
+        getOnboardStatus()
+    }
+
+    private fun getOnboardStatus() {
+        getOnboardStatusUseCase().onEach {completed ->
+
+            if (completed) {
+                _startDestination.value = "home"
+            } else {
+                _startDestination.value = "onboard"
             }
+
             _isLoading.value = false
-        }
+
+        }.launchIn(viewModelScope)
     }
 
 }
